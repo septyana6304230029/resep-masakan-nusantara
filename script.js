92a831c4dbbfdbe2f8dbd9422abadf41f959821e
Septyana Angelia Putri <6304230029@student.polbeng.ac.id>
2026-07-07 19:57:21 +0700

    Project awal resep masakan nusantara

---
+++ script.js
@@ -0,0 +1,59 @@
+function showRecipe(food) {
+
+    let recipe = "";
+
+    switch (food) {
+
+        case "rendang":
+            recipe =
+            "🍛 RENDANG\n\n" +
+            "Bahan:\n" +
+            "- 500 gram daging sapi\n" +
+            "- Santan\n" +
+            "- Cabai\n" +
+            "- Bawang merah\n" +
+            "- Bawang putih\n\n" +
+            "Cara Memasak:\n" +
+            "Masak semua bahan hingga kuah mengering dan bumbu meresap.";
+            break;
+
+        case "pempek":
+            recipe =
+            "🥟 PEMPEK\n\n" +
+            "Bahan:\n" +
+            "- Ikan tenggiri\n" +
+            "- Tepung sagu\n" +
+            "- Telur\n\n" +
+            "Cara Memasak:\n" +
+            "Campurkan bahan, bentuk adonan, lalu rebus hingga matang.";
+            break;
+
+        case "sate":
+            recipe =
+            "🍢 SATE MADURA\n\n" +
+            "Bahan:\n" +
+            "- Daging ayam\n" +
+            "- Bumbu kacang\n" +
+            "- Kecap manis\n\n" +
+            "Cara Memasak:\n" +
+            "Tusuk daging, bakar hingga matang, lalu sajikan dengan bumbu kacang.";
+            break;
+
+        case "mieaceh":
+            recipe =
+            "🍜 MIE ACEH\n\n" +
+            "Bahan:\n" +
+            "- Mi kuning\n" +
+            "- Daging sapi\n" +
+            "- Cabai\n" +
+            "- Bawang\n\n" +
+            "Cara Memasak:\n" +
+            "Tumis bumbu, masukkan daging dan mi, lalu aduk hingga matang.";
+            break;
+
+        default:
+            recipe = "Resep tidak ditemukan.";
+    }
+
+    alert(recipe);
+}
